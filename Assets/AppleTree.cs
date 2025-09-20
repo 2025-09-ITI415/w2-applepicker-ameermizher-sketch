72557fdbf5cc5d92db7bc1e8b3169b2d8d737df8
ameermizher-sketch <ameer.mizher@gmail.com>
2025-09-19 21:52:57 -0400

    Amir Mezher

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -30,3 +30,15 @@ public class AppleTree : MonoBehaviour {
          }
     }
 }
+public class AppleTree : MonoBehaviour {
+    // ...
+
+    void Update() {
+        // Basic Movement
+        Vector3 pos = transform.position;
+        pos.x += speed * Time.deltaTime;
+        transform.position = pos;
+
+        // Changing Direction
+    }
+}
