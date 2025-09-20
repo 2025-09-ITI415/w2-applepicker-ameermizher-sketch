36e53632926312f0cb33574a8110a692dabd46b8
ameermizher-sketch <ameer.mizher@gmail.com>
2025-09-19 21:18:49 -0400

    Amir Mezher

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -3,31 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class AppleTree : MonoBehaviour {
-    [Header("Inscribed")]                                                  // a
-    // Prefab for instantiating apples
-    public GameObject applePrefab;
-
-    // Speed at which the AppleTree moves
-    public float speed = 1f;
-
-    // Distance where AppleTree turns around
-    public float leftAndRightEdge = 10f;
-
-    // Chance that the AppleTree will change directions
-    public float changeDirChance = 0.1f;
-
-    // Seconds between Apples instantiations
-    public float appleDropDelay = 1f;
-
-    void Start () {
-        // Start dropping apples                                           // b
-    }
-
     void Update () {
-        Vector3 pos = transform.position;                        // b         pos.x += speed * Time.deltaTime;                         // c
-         transform.position = pos;                                // d
+        // Basic Movement
+        ...
+        // Changing Direction
+        if ( pos.x < -leftAndRightEdge ) {
+            speed = Mathf.Abs(speed);   // Move right
+        } else if ( pos.x > leftAndRightEdge ) {
+            speed = -Mathf.Abs(speed);  // Move left
+        } else if ( Random.value < changeDirChance ) {   // a
+            speed *= -1;  // Change direction             // b
+        }
+    }
+}
 
-         // Changing Direction
-     }
- }                                             // b
-    
+   void Start()
+
+{
+// start dropping Apples
+Invoke(&quot;DropApple&quot;, 2f);
+
+}
+
+void DropApple()
+{
+GameObject apple = Instantiate&lt;GameObject&gt;(applePrefab);
+apple.transform.position = transform.position;
+
+Invoke(&quot;DropApple&quot;,appleDropDelay);
+}
+
+void Update()
+
+// Basic Movement
+
+Vector3 pos = transform.position;
+pos.x += speed * Time.deltaTime;
+transform.position = pos;
+
+//changing direction
+
+if (pos.x &lt; -leftAndRightEdge)
+{
+speed = Mathf.Abs(speed); //Move right
+}                        // b
+else if (pos.x &gt; leftAndRightEdge)
+{
+speed = -Mathf.Abs(speed); // move left
+}
+//else if (Random.value &lt; changeDirChance)
+//{
+// speed *= -1; // change direction
+//}
+
+}
+
+void FixedUpdate()
+{
+// Random dorection changes are now time-based due to FixUpdate()
+if (Random.value &lt; changeDirChance)
+{
+speed *= -1; // change direction
+}
+
+}
